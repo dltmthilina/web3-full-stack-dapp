c8f7beb3c841effeb75abefe3da22e50536d3131
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-12-28 16:06:27 +0530

    change hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+
+task("accounts", "Prints the list of accounts", async(taskArgs, hre)=>{
+  const accounts = await hre.ethers.getSigners()
+
+  for(const account of accounts){
+    console.log(account.addrss);
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks:{
+    hardhat:{
+      chainId:1337
+    }
+  },
   solidity: "0.8.17",
 };
